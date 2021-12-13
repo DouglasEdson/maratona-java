f008d9fb2ab9a628193db3fc4e0258f6fa9ff784
DouglasEdson <douglas22@live.com.pt>
2021-12-13 19:26:30 -0300

    Aula08ArraysMultidimensionais02

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -5,32 +5,37 @@ public class Aula08ArraysMultidimensionais01 {
         // 1,2,3,4,5 Meses
         // 31, 28, 31, 30 dias
         int[][][] dias = new int[3][3][3];
-        dias [0][0][0] = 31;
-        dias [0][1][1] = 28;
-        dias [0][2][2] = 31;
+        dias[0][0][0] = 31;
+        dias[0][1][1] = 28;
+        dias[0][2][2] = 31;
 
-        dias [1][0][0] = 31;
-        dias [1][1][1] = 28;
-        dias [1][2][2] = 31;
+        dias[1][0][0] = 31;
+        dias[1][1][1] = 28;
+        dias[1][2][2] = 31;
 
-        dias [2][2][2] = 31;
-        dias [2][1][1] = 28;
-        dias [2][2][2] = 31;
+        dias[2][2][2] = 31;
+        dias[2][1][1] = 28;
+        dias[2][2][2] = 31;
 
         for (int i = 0; i < dias.length; i++) {
-            for (int j = 0; j < dias[0].length; j++) {
+            for (int j = 0; j < dias[i].length; j++) {
                 for (int k = 0; k < dias[0].length; k++) {
                     System.out.println(dias[i][j][k]);
 
                 }
-                
+
             }
-            
+            System.out.println("________________");
+
+        for (int[][] arrBase: dias){
+            for (int[] num: arrBase){
+                System.out.println(num);
+            }
         }
-        
+        }
 
 
-   }
+    }
 }
 
 
