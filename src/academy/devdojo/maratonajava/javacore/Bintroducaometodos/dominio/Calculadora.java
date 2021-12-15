f423823ae99503dbf1633772da9eeab9533b2b52
DouglasEdson <douglas22@live.com.pt>
2021-12-15 13:28:15 -0300

    Orientação Obejtos - Métodos pt 02 - Parâmetros

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -6,7 +6,12 @@ public class Calculadora {
         System.out.println(10+10);
     }
 
-    public static void subtraiDoisNumeros(){
+    public void subtraiDoisNumeros(){
         System.out.println(21-2);
+    }
+
+    public void multiplicaDoisNumeros(int num1, float num2){
+        System.out.println(num1 * num2);
+
     }
 }
