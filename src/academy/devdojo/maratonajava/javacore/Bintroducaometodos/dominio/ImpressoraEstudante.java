5ee187c867d062670079cbe8d9ca08ffd450c1a0
DouglasEdson <douglas22@live.com.pt>
2021-12-16 11:55:46 -0300

    Orientação Obejtos - Métodos pt06 - Parâmetros tipo referência

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class ImpressoraEstudante {
+    public void imprime(Estudante estudante) {
+        System.out.println(estudante.nome + "\n" + estudante.sexo + "\n" + estudante.idade + "\n");
+    }
+
+}
