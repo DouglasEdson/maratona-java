8cf81a1c67a3b3b283b5794d599afe1cd4fa72e3
DouglasEdson <douglas22@live.com.pt>
2021-12-17 09:31:21 -0300

    Orientação Objetos - Métodos pt 07 - Parâmetros tipo Referência

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -8,7 +8,8 @@ public class EstudanteTest01 {
     public static void main(String[] args) {
         Estudante estudante01 = new Estudante();
         Estudante estudante02 = new Estudante();
-        ImpressoraEstudante impressora  = new ImpressoraEstudante();
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
+
 
         estudante01.nome = "Midoriya";
         estudante01.idade = 15;
@@ -22,7 +23,11 @@ public class EstudanteTest01 {
 
         impressora.imprime(estudante02);
 
+        System.out.println("_________________________");
 
+        impressora.imprime(estudante01);
 
+        impressora.imprime(estudante02);
+        
     }
 }
