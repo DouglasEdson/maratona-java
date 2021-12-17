7c60eb5ce19bc6c331c48966f138419e623f37d6
DouglasEdson <douglas22@live.com.pt>
2021-12-17 09:46:46 -0300

    Orientação Objetos - Métodos pt 08 - Referência this

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest02.java
@@ -1,15 +1,11 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
 
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
-import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
 
-
-public class EstudanteTest01 {
+public class EstudanteTest02 {
     public static void main(String[] args) {
         Estudante estudante01 = new Estudante();
         Estudante estudante02 = new Estudante();
-        ImpressoraEstudante impressora = new ImpressoraEstudante();
-
 
         estudante01.nome = "Midoriya";
         estudante01.idade = 15;
@@ -19,15 +15,8 @@ public class EstudanteTest01 {
         estudante02.idade = 16;
         estudante02.sexo = 'F';
 
-        impressora.imprime(estudante01);
-
-        impressora.imprime(estudante02);
-
-        System.out.println("_________________________");
-
-        impressora.imprime(estudante01);
-
-        impressora.imprime(estudante02);
+        estudante01.imprime();
+        estudante02.imprime();
 
     }
 }
